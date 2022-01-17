1d38fb2102f57eb21516f51cd7bdb90a525b51f5
mike <jobles11@gmail.com>
2022-01-17 12:46:59 +0300

    update main test string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Huffman h1 = new Huffman();
-        h1.buildHuffmanTree("Huffman coding is a data compression algorithm.");
+        h1.buildHuffmanTree("algorithm data compression by Huffman coding is am.");
     }
 }
